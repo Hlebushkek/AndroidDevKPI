7426f578bab6add972630b07b039b87cfbaf8aff
Hlib Sobolevskyi <volsoor@gmail.com>
2022-10-07 01:18:20 +0300

    remove unsued

--- Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -3,11 +3,8 @@ package com.example.lab1
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.lab1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
